5c5ca678443a65ea9bcb2b3c9a13b53d0a964e0a
Vivek Kumar <vnjvibhash@gmail.com>
2025-08-03 04:03:45 +0530

    Profile and splash mofified

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -269,25 +269,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  void _showPopupDialog(BuildContext context, String title, Widget content) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: SingleChildScrollView(
-            child: content,
-          ),
-          actions: [
-            TextButton(
-              child: const Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 }
--- lib/widgets/custom_container.dart
+++ lib/widgets/custom_container.dart
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-
-class CustomContainer extends StatelessWidget {
-  final Widget child;
-  final double width;
-  final double padding;
-  final EdgeInsets? margin;
-
-  const CustomContainer({
-    Key? key,
-    required this.child,
-    required this.width,
-    required this.padding,
-    this.margin,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: margin ?? const EdgeInsets.only(bottom: 8.0),
-      padding: EdgeInsets.all(padding), // Use padding from the constructor
-      width: width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: child, // Use the passed child widget
-    );
-  }
-}
-=======
 import 'package:flutter/material.dart';
 
 class CustomContainer extends StatelessWidget {
@@ -76,4 +36,3 @@ class CustomContainer extends StatelessWidget {
     );
   }
 }
->>>>>>> b9a47fbbf7aa17fddb8494dbb96a7c0cefed7c40
